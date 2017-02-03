38946f6183ebe1b31caf4c9f033b1811b985a7b0
Lucas Kacher <lucas@vsco.co>
2017-02-03 14:33:04 -0500

    Add failing test for current crc % 100 comparison `isAvailableForId`

--- test/index.js
+++ test/index.js
@@ -18,7 +18,8 @@ var mockFeatures = {
       default: {
         boolean_feature: true,
         boolean_feature_off : false,
-        percentile_feature : 0.5
+        percentile_feature : 0.5,
+        percentile_feature_off : 0
       }
     }
   }
@@ -52,6 +53,27 @@ describe('Dcdr', function() {
     expect(dcdr.isAvailableForId('percentile_feature', 2)).to.be.true;
     expect(dcdr.isAvailableForId('percentile_feature', 8)).to.be.false;
     expect(dcdr.isAvailableForId('nonexistent_feature', 6)).to.be.false;
+    done();
+  });
+
+  /**
+   * A test to guard against the crc function hashing specific values which
+   * ultimatley end in `00`, and therefore would return true for `isAvailableForId`
+   * lookups since the comparison operator was mistakenly set to `<=` versus `<`
+   */
+  it('handles percentiles with specific hashes that trick the crc % function', function(done) {
+    dcdr.setFeatures(mockFeatures);
+    expect(dcdr.isAvailableForId('percentile_feature_off', 231917551091711)).to.be.false;
+    expect(dcdr.isAvailableForId('percentile_feature_off', 3945464305025023)).to.be.false;
+    expect(dcdr.isAvailableForId('percentile_feature_off', 3945464305025023)).to.be.false;
+    expect(dcdr.isAvailableForId('percentile_feature_off', 669979974303743)).to.be.false;
+    expect(dcdr.isAvailableForId('percentile_feature_off', 7717664547930111)).to.be.false;
+    done();
+  });
+
+  it('handles percentiles with really large ID values', function(done) {
+    dcdr.setFeatures(mockFeatures);
+    expect(dcdr.isAvailableForId('percentile_feature_off', Number.MAX_SAFE_INTEGER)).to.be.false;
     done();
   });
 
